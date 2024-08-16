0a6a04544611a0a897896fdb17f8e6a06b355be0
Sam Weston <11150054+cablespaghetti@users.noreply.github.com>
2024-08-16 15:27:27 +0100

    Only work on wednesdays

--- wednesday-api/app.py
+++ wednesday-api/app.py
@@ -1,13 +1,21 @@
 import csv
 import random
+from datetime import datetime
 
-from flask import Flask, jsonify, make_response
+from flask import Flask, jsonify, make_response, request
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def root():
+    fake_wednesday = request.args.get('it_is_wednesday_in_my_heart')
+    if not fake_wednesday:
+        today = datetime.now()
+        if today.weekday() != 2:
+            return jsonify(message="It is not Wednesday"), 425
+
+    # Read in the CSV to a list of dicts
     wednesday_list = []
     with open("wednesday.csv", newline="") as csvfile:
         reader = csv.DictReader(
@@ -15,7 +23,11 @@ def root():
         )
         for row in reader:
             wednesday_list.append(dict(row))
+
+    # Select a random quote
     random_quote = random.choice(wednesday_list)
+
+    # Return the random quote
     return jsonify(quote=random_quote["quote"], attribution=random_quote["attribution"])
 
 
