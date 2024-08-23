a80c5421ce661bcec9e947a4affc5ac9747e5118
Sam Weston <11150054+cablespaghetti@users.noreply.github.com>
2024-08-23 10:57:02 +0100

    Do setup and teardown properly

--- tests/utils/test_utils.py
+++ tests/utils/test_utils.py
@@ -1,21 +1,29 @@
 import os
 import uuid
 
+import pytest
+
 from utils.utils import load_quotes
 
 
-def test_load_quoes():
-    """
-    Check that quotes get loaded
-    """
+@pytest.fixture
+def setup_quote_file():
     temp_path = "/tmp/" + str(uuid.uuid4())
     with open(temp_path, "a") as temp_file:
         temp_file.write("quote number one;author\n")
         temp_file.write("quote number two;author two\n")
-    quotes = load_quotes(temp_path)
+    yield temp_path
+    os.remove(temp_path)
+
+
+def test_load_quoes(setup_quote_file):
+    """
+    Check that quotes get loaded
+    """
+
+    quotes = load_quotes(setup_quote_file)
     expected_list = [
         {"quote": "quote number one", "attribution": "author"},
         {"quote": "quote number two", "attribution": "author two"},
     ]
     assert quotes == expected_list
-    os.remove(temp_path)
